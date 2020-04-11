739202cd8a604859f51c6fb92e2eb201ee4f82da
Kristof Klein <kkl@Kristofs-iMac.local>
2020-04-11 17:46:15 +0200

    Add placeholder for new UI
    As .Net Core is used a new platform independent UI is needed.

---
+++ C3UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C3UI.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+    }
+}
