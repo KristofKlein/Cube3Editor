f84fa84ec037a91266ba446902f48898e12fa5c6
Kristof Klein <kkl@Kristofs-iMac.local>
2020-04-11 20:18:42 +0200

    add ScriptGenerator

--- Cube3ScriptGenerator/c3sg.cs
+++ Cube3ScriptGenerator/c3sg.cs
@@ -10,18 +10,17 @@ using Org.BouncyCastle.Crypto.Engines;
 using Org.BouncyCastle.Crypto.Paddings;
 using Org.BouncyCastle.Crypto.Parameters;
 
-
 namespace Cube3ScriptGenerator
 {
-    class c3sg
+    internal static class C3sg
     {
-        private static string key = "221BBakerMycroft";
+        private const string key = "221BBakerMycroft";
 
         private static Byte[] inputCubeFile;
         private static CubeExtractor extractor;
         private static Byte[] dataModel;
 
-        public static BlowfishEngine engine { get; private set; }
+        public static BlowfishEngine Engine { get; private set; }
 
         public static void Main(string[] args)
         {
@@ -35,14 +34,9 @@ namespace Cube3ScriptGenerator
                 {
                     scriptFile = args[1];
                 }
-
             }
 
-            if (cube3File == null ||
-                cube3File.ToLower().Equals("-help") ||
-                cube3File.ToLower().Equals("/help") ||
-                cube3File.ToLower().Equals("-?") ||
-                cube3File.ToLower().Equals("/?"))
+            if (cube3File?.Equals("-help", StringComparison.OrdinalIgnoreCase) != false || cube3File.Equals("-?", StringComparison.OrdinalIgnoreCase))
             {
                 System.Console.WriteLine("usage: c3sg <cube3File> [<scriptFile>]");
                 System.Console.WriteLine("");
@@ -52,18 +46,19 @@ namespace Cube3ScriptGenerator
                 System.Console.WriteLine("");
                 Environment.Exit(0);
             }
+            else
+            {
+            }
 
             if (scriptFile == null)
             {
                 scriptFile = Path.GetFileNameWithoutExtension(cube3File) + ".CUBESCR";
             }
 
-            engine = new BlowfishEngine();
-            engine.UseLittleEndian = true;
-            generateScriptFromCube3File(cube3File, scriptFile);
-
+            Engine = new BlowfishEngine();
+            GenerateScriptFromCube3File(cube3File, scriptFile);
         }
-        private static void generateScriptFromCube3File(String FileName, String scriptFile)
+        private static void GenerateScriptFromCube3File(String FileName, String scriptFile)
         {
             Encoding encoding = Encoding.ASCII;
             String decodedModel;
@@ -75,78 +70,73 @@ namespace Cube3ScriptGenerator
             {
                 try
                 {
-                    using (var inFile = File.OpenRead(FileName))
-                    using (var binaryReader = new BinaryReader(inFile))
+                    using var inFile = File.OpenRead(FileName);
+                    using var binaryReader = new BinaryReader(inFile);
+                    inputCubeFile = binaryReader.ReadBytes((int)binaryReader.BaseStream.Length);
+                    bool copyInputFile = true;
+                    if (!RawCubeFile())
                     {
-                        inputCubeFile = binaryReader.ReadBytes((int)binaryReader.BaseStream.Length);
-                        bool copyInputFile = true;
-                        if (!RawCubeFile())
-                        {
-                            extractor = new CubeExtractor(inputCubeFile);
+                        extractor = new CubeExtractor(inputCubeFile);
 
-                            String rawCube3Filename = extractor.GetCubeFilename();
-                            if (rawCube3Filename != null)
-                            {
-                                copyInputFile = false;
-                                dataModel = new byte[extractor.ModelFiles[rawCube3Filename].Length];
-                                Array.Copy(extractor.ModelFiles[rawCube3Filename], dataModel, extractor.ModelFiles[rawCube3Filename].Length);
-                            }
+                        String rawCube3Filename = extractor.GetCubeFilename();
+                        if (rawCube3Filename != null)
+                        {
+                            copyInputFile = false;
+                            dataModel = new byte[extractor.ModelFiles[rawCube3Filename].Length];
+                            Array.Copy(extractor.ModelFiles[rawCube3Filename], dataModel, extractor.ModelFiles[rawCube3Filename].Length);
+                        }
+                    }
+
+                    if (copyInputFile)
+                    {
+                        dataModel = new byte[inputCubeFile.Length];
+                        Array.Copy(inputCubeFile, dataModel, inputCubeFile.Length);
+                    }
+                    try
+                    {
+                        ZeroBytePadding padding = new ZeroBytePadding();
+                        cipher = new PaddedBufferedBlockCipher(Engine, padding);
+
+                        // make sure buffer is a multiple of Blowfish Block size.
+                        int leftover = dataModel.Length % cipher.GetBlockSize();
+                        if (leftover != 0)
+                        {
+                            Array.Resize(ref dataModel, dataModel.Length + (cipher.GetBlockSize() - leftover));
                         }
 
-                        if (copyInputFile)
+                        // create the key parameter
+                        Byte[] keyBytes = encoding.GetBytes(key);
+                        KeyParameter param = new KeyParameter(encoding.GetBytes(key));
+
+                        // initalize the cipher
+                        cipher.Init(false, new KeyParameter(keyBytes));
+
+                        byte[] decodedBytes = new byte[cipher.GetOutputSize(dataModel.Length)];
+
+                        int decodedLength = cipher.ProcessBytes(dataModel, 0, dataModel.Length, decodedBytes, 0);
+                        if (decodedLength % 8 == 0)
+                            cipher.DoFinal(decodedBytes, decodedLength);
+
+                        decodedModel = encoding.GetString(decodedBytes);
+
+                        bfbObject = new BitFromByte(encoding, decodedBytes);
+
+                        CubeScript cscript = new CubeScript(bfbObject);
+
+                        System.IO.StreamWriter file = new System.IO.StreamWriter(scriptFile, false);
+
+                        foreach (String line in cscript.CubeScriptLines)
                         {
-                            dataModel = new byte[inputCubeFile.Length];
-                            Array.Copy(inputCubeFile, dataModel, inputCubeFile.Length);
+                            file.WriteLine(line);
                         }
 
-
-                        try
-                        {
-                            ZeroBytePadding padding = new ZeroBytePadding();
-                            cipher = new PaddedBufferedBlockCipher(engine, padding);
-
-                            // make sure buffer is a multiple of Blowfish Block size.
-                            int leftover = dataModel.Length % cipher.GetBlockSize();
-                            if (leftover != 0)
-                            {
-                                Array.Resize(ref dataModel, dataModel.Length + (cipher.GetBlockSize() - leftover));
-                            }
-
-                            // create the key parameter 
-                            Byte[] keyBytes = encoding.GetBytes(key);
-                            KeyParameter param = new KeyParameter(encoding.GetBytes(key));
-
-                            // initalize the cipher
-                            cipher.Init(false, new KeyParameter(keyBytes));
-
-                            byte[] decodedBytes = new byte[cipher.GetOutputSize(dataModel.Length)];
-
-                            int decodedLength = cipher.ProcessBytes(dataModel, 0, dataModel.Length, decodedBytes, 0);
-                            if (decodedLength % 8 == 0)
-                                cipher.DoFinal(decodedBytes, decodedLength);
-
-                            decodedModel = encoding.GetString(decodedBytes);
-
-                            bfbObject = new BitFromByte(encoding, decodedBytes);
-
-                            CubeScript cscript = new CubeScript(bfbObject);
-
-                            System.IO.StreamWriter file = new System.IO.StreamWriter(scriptFile, false);
-
-                            foreach(String line in cscript.CubeScriptLines)
-                            {
-                                file.WriteLine(line);
-                            }
-
-                            file.Close();
-
-                        }
-                        catch (IOException)
-                        {
-                            Console.WriteLine($"Unable to process CUBE3 File [{FileName}]\n\n" +
-                                $"Was this really a CUBE3 file?");
-                            Environment.Exit(-1);
-                        }
+                        file.Close();
+                    }
+                    catch (IOException)
+                    {
+                        Console.WriteLine($"Unable to process CUBE3 File [{FileName}]\n\n" +
+                            $"Was this really a CUBE3 file?");
+                        Environment.Exit(-1);
                     }
                 }
                 catch (SecurityException ex)
@@ -174,8 +164,5 @@ namespace Cube3ScriptGenerator
             }
             return raw;
         }
-
     }
-
-
 }
